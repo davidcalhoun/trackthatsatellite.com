bce0148e6617c85dc0bad2e6eb00f4f395b314bf
David Calhoun <david.b.calhoun@gmail.com>
2017-10-06 17:40:17 -0700

    update tle script: fix typo, combine all files, extract iss into own file (to be bundled with main JS payload)

--- update-celestrak-cache.sh
+++ update-celestrak-cache.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 BASE_URL="https://celestrak.com/NORAD/elements/"
-
 OUTPUT_DIR="src/js/tles/"
 
 declare -a files=("tle-new"
@@ -38,20 +37,29 @@ declare -a files=("tle-new"
   "science"
   "geodetic"
   "engineering"
-  "eduation"
+  "education"
   "military"
   "radar"
   "cubesat"
   "other"
 )
 
+# Fetch each file.
 declare -i i=1
-
 for filename in "${files[@]}"
 do
   echo "Progress: ${i}/${#files[@]}: ${filename}"
   curl "${BASE_URL}${filename}.txt" > "${OUTPUT_DIR}${filename}.txt"
-  # Sleep for a bit - don't hit the server too hard
+  # Sleep for a bit to be nice to the Celestrak servers.
   sleep .5
   let i++
 done
+
+# Combine all TLEs.
+cd "${OUTPUT_DIR}"
+rm _all-tles.txt || true
+rm _iss.txt || true
+cat * >> _all-tles.txt
+
+# Extract ISS info to bundle with initial JS payload.
+head -3 stations.txt >> _iss.txt
