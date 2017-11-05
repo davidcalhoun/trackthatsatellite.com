bd8c97fe23fb39ed21657cdfbc0fe86a78c066a2
David Calhoun <david.b.calhoun@gmail.com>
2017-11-05 13:44:18 -0800

    webpack: add more stuff to external scripts, add config for bundle analyzer

--- webpack.config.js
+++ webpack.config.js
@@ -68,7 +68,9 @@ module.exports = {
   // Don't bundle these, as they're brought in through CDN <script> tags.
   externals: {
     'react': 'React',
-    'react-dom': 'ReactDOM'
+    'react-dom': 'ReactDOM',
+    'react-redux': 'ReactRedux',
+    'react-router': 'ReactRouter'
   },
 
   // Filetype-specific handling.
@@ -200,6 +202,32 @@ module.exports = {
     // Displays Webpack progress bar on command line.
     new SimpleProgressPlugin(),
 
-    new BundleAnalyzerPlugin()
+    new BundleAnalyzerPlugin({
+      // Can be `server`, `static` or `disabled`.
+      // In `server` mode analyzer will start HTTP server to show bundle report.
+      // In `static` mode single HTML file with bundle report will be generated.
+      // In `disabled` mode you can use this plugin to just generate Webpack Stats JSON file by setting `generateStatsFile` to `true`.
+      analyzerMode: 'static',
+      // Path to bundle report file that will be generated in `static` mode.
+      // Relative to bundles output directory.
+      reportFilename: 'webpack-report.html',
+      // Module sizes to show in report by default.
+      // Should be one of `stat`, `parsed` or `gzip`.
+      // See "Definitions" section for more information.
+      defaultSizes: 'parsed',
+      // Automatically open report in default browser
+      openAnalyzer: true,
+      // If `true`, Webpack Stats JSON file will be generated in bundles output directory
+      generateStatsFile: false,
+      // Name of Webpack Stats JSON file that will be generated if `generateStatsFile` is `true`.
+      // Relative to bundles output directory.
+      statsFilename: 'stats.json',
+      // Options for `stats.toJson()` method.
+      // For example you can exclude sources of your modules from stats file with `source: false` option.
+      // See more options here: https://github.com/webpack/webpack/blob/webpack-1/lib/Stats.js#L21
+      statsOptions: null,
+      // Log level. Can be 'info', 'warn', 'error' or 'silent'.
+      logLevel: 'info'
+    })
   ]
 };
