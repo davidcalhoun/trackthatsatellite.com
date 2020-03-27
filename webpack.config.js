b549fbd920d6c3082264f2dec31413a75d5ca30d
David Calhoun <david.b.calhoun@gmail.com>
2020-03-27 18:57:24 -0400

    configs: add postcss-global-import

--- webpack.config.js
+++ webpack.config.js
@@ -56,6 +56,7 @@ const config = {
             options: {
               ident: "postcss",
               plugins: () => [
+                require("postcss-global-import"),
                 postcssCustomMedia({
                   importFrom: path.resolve(__dirname, "src/shared.css")
                 }),
