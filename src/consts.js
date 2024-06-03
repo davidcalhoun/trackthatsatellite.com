4e452c2c43f25f348e190f12feef9b88782fa8c2
David Calhoun <dcalhoun@methanesat.org>
2024-06-03 15:53:21 -0400

    consts: update to domain-locked mapbox access token; Prettier formatting

--- src/consts.js
+++ src/consts.js
@@ -1,17 +1,18 @@
-export const siteName = "Track That Satellite!"
+export const siteName = "Track That Satellite!";
 
 export const defaultSat = "iss-zarya-25544";
 
 export const defaultSatId = "25544";
 
-export const MAPBOX_ACCESS_TOKEN = "pk.eyJ1IjoiZnJhbmtzdmFsbGkiLCJhIjoiSFd2NDgxdyJ9.86RUhUDNQtjWdZysxDIzuw";
+export const MAPBOX_ACCESS_TOKEN =
+  "pk.eyJ1IjoiZnJhbmtzdmFsbGkiLCJhIjoiY2lndDhibHVsMDZ2aXRwa25jbG45eTVuMiJ9.NTSjn-FzJP5NbDOQjXxZaw";
 
 export const TLE_SOURCE_URL = "https://www.trackthatsatellite.com/data/tles.txt";
 
 export const BREAKPOINTS = {
-	1: 100,
-	4: 480,
-	7: 768,
-	9: 992,
-	12: 1200
-}
+  1: 100,
+  4: 480,
+  7: 768,
+  9: 992,
+  12: 1200,
+};
